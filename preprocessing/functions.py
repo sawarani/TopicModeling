e57aff5913657b24e4d7e6adab1b037e1198ea71
sawarani <sawarani>
2023-06-02 15:20:33 +0300

    update functions.py

--- preprocessing/functions.py
+++ preprocessing/functions.py
@@ -1,14 +1,15 @@
 import os
 import re
-import json
 import spacy
 import string
 import pymorphy2
+import pandas as pd
 from gensim.models import Phrases
 from nltk.tokenize import word_tokenize
 from spacy.tokenizer import Tokenizer
-from spacy.util import compile_prefix_regex, compile_infix_regex, compile_suffix_regex
+from spacy.util import compile_prefix_regex, compile_suffix_regex
 from spacy.pipeline import merge_entities
+
 
 # Do NOT split intra-hyphen words (spaCy)
 def custom_tokenizer(nlp):
@@ -20,25 +21,23 @@ def custom_tokenizer(nlp):
                      infix_finditer=infix_re.finditer, \
                      token_match=None)
 
-def read_texts(n):
+def read_texts(sources):
     dirpath = os.path.dirname(os.getcwd()) + '\\corpus\\'
-    js_docs = []
-    with open(dirpath+'elementy_texts.json', 'r') as f:
-        js = json.load(f)
-        for key in js:
-            if len(key) > n: # no more than n texts per author
-                num = n
-            else:
-                num = len(js[key])
-            add_texts_to_json(js, num, key, js_docs)
-    return js_docs
+    dfs = []
+    for source in sources:
+        df = pd.read_json(dirpath + source)
+        df['Full text'] = df['Full text'].str.replace(r'[\xad…]', '', \
+                                                      regex=True).astype('str') # soft hyphen
+        df['Full text'] = df['Full text'].str.replace(r'[\xa0]', ' ', \
+                                                      regex=True).astype('str') # no-break space
+        df['Full text'] = df['Full text'].str.replace('ё', 'е').astype('str') # normalize spelling
+        df = df[df['Author'] != 'N/A'] # remove texts without the author
+        df = df[df['Topics'] != ''] # remove texts with no pre-defined topics
+        dfs.append(df)
+    return pd.concat(dfs, ignore_index=True)
 
-def add_texts_to_json(json, n, name, texts):
-    for i in range(n):
-        text = json[name][i]['Full text']
-        no_hyphen = re.sub('[\xad…]', '', text) # soft hyphen character          
-        no_space = re.sub('[\xa0]', ' ', no_hyphen) # no-break space
-        texts.append(no_space)
+def get_tokens(texts):
+    return [word_tokenize(text) for text in texts]
 
 def get_lemmas(texts):
     return [[morph.normal_forms(word)[0] if '_' not in word \
@@ -46,20 +45,20 @@ def get_lemmas(texts):
              for word in text] for text in texts]
 
 def get_named_ents(texts):
+    ner_types = ['ORG', 'PERSON', 'GPE', 'LOC']
     data = []
     for text in texts:
-        data.append([str(word) if not word.ent_type_ else str(word).replace(' ', '_') \
-                     for word in nlp(text)])
+        data.append([str(word).replace(' ', '_') if word.ent_type_ and word.ent_type_ in ner_types \
+                     else str(word) for word in nlp(text)])
     return data
 
-def get_ngrams(texts):
-    ngram = Phrases(texts, min_count=10, threshold=100)
+def get_ngrams(texts, min_count, threshold):
+    ngram = Phrases(texts, min_count=min_count, threshold=threshold)
     return ngram[texts]
 
 def get_nouns_adj(texts):
-    return [[word for word in text \
-             if morph.parse(word)[0].tag.POS == 'NOUN' \
-             or morph.parse(word)[0].tag.POS == 'ADJF'] \
+    pos = ['NOUN', 'ADJF']
+    return [[word for word in text if morph.parse(word)[0].tag.POS in pos] \
             for text in texts]
 
 def clean_texts(texts):
@@ -67,25 +66,17 @@ def clean_texts(texts):
     punct += '—“”«»<>…...°1234567890'
     remove = punct.replace('_', '').replace('-', '') # do not remove
     pattern = r"[{}]".format(remove)
-    data = [[re.sub(pattern, '', word).strip('_-') for word in text] \
+    data = [[re.sub(pattern, '', word).rstrip('_-') for word in text] \
             for text in texts]
-    return [[word.replace('__', '_') for word in text if word] \
+    # ignore words that contain only latin characters
+    return [[re.sub('[Ёё]', 'е', word) for word in text if word and re.search(r'[^a-zA-Z]+', word)] \
             for text in data]
 
 def remove_stopwords(texts):
     with open('swl_optimum.txt', 'r', encoding='utf-8') as f:
         stopwords = f.read().split('\n')
-        data = [[word for word in text if re.sub('[Ёё]', 'е', word) \
-                 not in stopwords or word not in stopwords] for text in texts]
-        # ignore words that contain only latin characters
-        return [[word for word in text if re.search(r'[^a-zA-Z]+', word)] \
-                for text in data]
-
-def get_corpus_size(texts):
-    tokens = [word_tokenize(doc) for doc in texts]
-    tokens_clean = clean_texts(tokens)
-    data = [[word for word in text if word] for text in tokens_clean]
-    return sum([len(token) for token in data])
+    data = [[word for word in text if word not in stopwords] for text in texts]
+    return [[word for word in text if word] for text in data]
 
 
 # Load spaCy model
